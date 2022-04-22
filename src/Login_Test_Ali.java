b5c1000727c7b8f0db313bd12f9d43bc6f6bbaaf
Ali SARAYDAR <saraydar.ali@gmail.com>
2022-04-22 17:23:21 +0300

    AC2 completed

--- src/Login_Test_Ali.java
+++ src/Login_Test_Ali.java
@@ -2,5 +2,6 @@ public class Login_Test_Ali {
     public static void main(String[] args) {
         System.out.println("Initial changes are done and my Login class is created");
         System.out.println("AC1 testing");
+        System.out.println("AC2 testing");
     }
 }
